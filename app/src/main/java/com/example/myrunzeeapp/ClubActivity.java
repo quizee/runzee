6f94178f94c22f942960971ef6f0d701a0a09f1f
quizee <quiz22295@gmail.com>
2019-08-30 00:53:07 +0900

    refactoring

--- app/src/main/java/com/example/myrunzeeapp/ClubActivity.java
+++ app/src/main/java/com/example/myrunzeeapp/ClubActivity.java
@@ -27,6 +27,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
@@ -51,7 +52,7 @@ public class ClubActivity extends MenuActivity {
     //두가지 종류의 친구목록이 떠야한다. 추천친구와 친구검색
 
     //추천 친구
-    ArrayList<FriendItem> recommend_friends = new ArrayList<>();
+    ArrayList<FriendItem> recommendFriends = new ArrayList<>();
     RecyclerView crew_recycler;
     FriendListAdapter recommend_adapter;
 
@@ -93,7 +94,7 @@ public class ClubActivity extends MenuActivity {
 
         //추천 친구 세팅
         crew_recycler = findViewById(R.id.crew_recycler);
-        recommend_adapter = new FriendListAdapter(ClubActivity.this, recommendMap, recommend_friends);
+        recommend_adapter = new FriendListAdapter(ClubActivity.this, recommendMap, recommendFriends);
         layoutManager_recommend = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
         crew_recycler.setLayoutManager(layoutManager_recommend);
         crew_recycler.setAdapter(recommend_adapter);
@@ -109,63 +110,84 @@ public class ClubActivity extends MenuActivity {
         setTabLayout(0);
         group_lt.setImageResource(R.drawable.ic_group_black_24dp);
 
-        //전체 유저 목록과 내 친구목록을 가져온다.
-//        database.getReference().child("userlist").addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                Log.e(TAG, "onDataChange: "+"호출!!!!!");
-//                myFriendList.clear();
-//                totalUserList.clear();
-//                //변화가 있으면 새로 넣어야하므로 비우고 시작한다
-//                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-//                    //snapshot 하나당 한사람
-//                    Log.e(TAG, "onDataChange: " +snapshot);
-//                    if(snapshot.getKey().equals(auth.getCurrentUser().getUid())){
-//                        Log.e(TAG, "onDataChange: if문 1"+snapshot);
-//                        myFriendList = dataSnapshot.child("friendList").getValue(type);//내 친구 리스트
-//                    }else{
-//                        Log.e(TAG, "onDataChange: if문 1"+snapshot);
-//                        totalUserList.add(snapshot.getValue(UserDTO.class));//전체 리스트
-//                    }
-//                }
-//                Log.e(TAG, "onDataChange: 친구리스트"+myFriendList.size()+" 전체리스트"+totalUserList.size());
-//            }
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
+    }
+
+    public void addCandidate(DataSnapshot dataSnapshot){
+        Log.e(TAG, "onChildAdded: 내친구가 아닌 사람들의 모임입니다" );
+
+        UserDTO searchDTO = dataSnapshot.getValue(UserDTO.class);
+        FriendItem newFriend = new FriendItem(searchDTO.name, searchDTO.profileUrl, searchDTO.email, searchDTO.uid);
+        friendItems.add(newFriend);
+        search_adapter.notifyDataSetChanged();
+
+        Log.e(TAG, "후보 "+friendItems.size() + searchDTO.name);
+    }
+
+    public void makeFriendMap(UserDTO recomDTO, String myUid){
+        for(String yourFriendUid : recomDTO.friendList){
+            if(!yourFriendUid.equals(myUid) && !myFriendList.contains(yourFriendUid)){//그 친구가 나 이거나 이미 내 친구라면 제외함
+                if (recommendMap.containsKey(yourFriendUid)) {//이미 있다면
+                    int count = recommendMap.get(yourFriendUid);
+                    count++; //출현 횟수를 한번 늘린다.
+                    Log.e(TAG, recomDTO.name+"씨 친구목록 안에 있습니다. "+ yourFriendUid+" "+count);
+                    recommendMap.put(yourFriendUid, count);
+                } else {//아직 없다면
+                    recommendMap.put(yourFriendUid, 1);
+                    Log.e(TAG, recomDTO.name+"씨 친구목록 안에 있습니다. "+ yourFriendUid+" "+1);
+                    //최초로 등장했으므로 출현 횟수는 1
+                }
+                Log.e(TAG, "여기까지 완성된 해시맵: "+recommendMap);
+                //이게 변경이 되는지는 모르겠다
+            }
+        }
+    }
+
+    public void sortFriends(){
+        FriendItem temp ;
+        for(int i = recommendFriends.size()-1; i>0; i--){
+            for(int j = 0; j<i; j++){
+                if(recommendMap.get(recommendFriends.get(j).uid)<recommendMap.get(recommendFriends.get(j+1).uid)){
+                    temp = recommendFriends.get(j);
+                    recommendFriends.remove(j);
+                    recommendFriends.add(j+1, temp);
+                }
+            }
+        }
+    }
+
+    public void putSearchedWord(ArrayList<FriendItem> found, String newText){
+        for(FriendItem item: friendItems){
+            if(newText.contains("@")) {
+                String search_words[];
+                search_words = newText.split("@");//검색어에 @ 가 포함되면 앞부분까지만 본다.
+                newText = search_words[0];
+            }
+            String results[] = item.email.split("@");//각 사용자의 이메일도 앞부분까지만 본다
+            if(item.name.contains(newText) || results[0].contains(newText)){//이메일에 검색어가 포함되거나 이름에 포함되면 추가
+                found.add(item);
+            }
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         recommendMap.clear();
-        recommend_friends.clear();
+        recommendFriends.clear();
         friendItems.clear();
         myFriendList.clear();
         totalUserList.clear();
-/*
 
-       database.getReference().child("userlist").child(auth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //snapshot 하나당 한명이다
-                myFriendList = dataSnapshot.child("friendList").getValue(type);
-                Log.e(TAG, "내 친구들 목록:"+myFriendList.size()+"구비 완료!");
-           }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
-       });
- */
+        DatabaseReference userListRef = database.getReference().child("userlist");//유저들의 목록
+        final String myUid = auth.getCurrentUser().getUid();//나의 uid
+        DatabaseReference myFriendListRef = userListRef.child(myUid).child("friendList");//내 친구목록
 
-       database.getReference("userlist").child(auth.getCurrentUser().getUid()).child("friendList").addChildEventListener(new ChildEventListener() {
+        myFriendListRef.addChildEventListener(new ChildEventListener() {
            @Override
            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                myFriendList.add(dataSnapshot.getValue(String.class));
                if(myFriendList!=null) {
-                   Log.e(TAG, "onChildAdded: " + "내친구 구햇따." + myFriendList.size());
+                   Log.e(TAG, "onChildAdded: " + "내친구 구했다." + myFriendList.size());
                }else{
                    Log.e(TAG, "onChildAdded: 아직 친구 없음");
                }
@@ -173,209 +195,66 @@ public class ClubActivity extends MenuActivity {
 
            @Override
            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
            }
 
            @Override
            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
            }
 
            @Override
            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
            }
 
            @Override
            public void onCancelled(@NonNull DatabaseError databaseError) {
-
            }
        });
 
-        //내 친구리스트를 먼저 가져온다.
-        /*
-        database.getReference().child("userlist").child(auth.getCurrentUser().getUid())
-                .child("friendList")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        myFriendList = dataSnapshot.getValue(type);
-                        if(myFriendList!=null) {
-                            Log.e(TAG, "onDataChange: " + "내친구 구햇따." + myFriendList.size());
-                        }
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-        //전체 리스트를 가져온다.
-        database.getReference().child("userlist").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    totalUserList.add(snapshot.getValue(UserDTO.class));
-                }
-                Log.e(TAG, "onDataChange: 전체리스트 구햇따"+totalUserList.size());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });*/
-//위에꺼 안되면 아래꺼로 걍 하자
-//        database.getReference().child("userlist").addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                Log.e(TAG, "차일드 들어왔다 add " +dataSnapshot);
-//                if(dataSnapshot.getKey().equals(auth.getCurrentUser().getUid())){
-//                    Log.e(TAG, ": if문 1"+dataSnapshot);
-//                    myFriendList = dataSnapshot.child("friendList").getValue(type);//내 친구 리스트
-//                }else{
-//                    Log.e(TAG, " if문 1"+dataSnapshot);
-//                    totalUserList.add(dataSnapshot.getValue(UserDTO.class));//전체 리스트
-//                }
-//                Log.e(TAG, "onDataChange: 친구리스트"+myFriendList.size()+" 전체리스트"+totalUserList.size());
-//            }
-//
-//            @Override
-//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//            }
-//
-//            @Override
-//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-
         //사람들 목록과 total friendlist를 만든다.
-
-        database.getReference().child("userlist").addChildEventListener(new ChildEventListener() {
+        userListRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull final DataSnapshot dataSnapshot, @Nullable String s) {
                 //검색했을 때 나오는 사람들 목록
-                if(!dataSnapshot.getKey().equals(auth.getCurrentUser().getUid()) && !myFriendList.contains(dataSnapshot.getKey())) {//나 자신과 이미 있는 친구 제외
-                    Log.e(TAG, "onChildAdded: 내친구가 아닌 사람들의 모임입니다" );
-                    UserDTO searchDTO = dataSnapshot.getValue(UserDTO.class);
-                    friendItems.add(new FriendItem(searchDTO.name, searchDTO.profileUrl, searchDTO.email, searchDTO.uid));
-                    search_adapter.notifyDataSetChanged();
-                    Log.e(TAG, "후보 "+friendItems.size() + searchDTO.name);
-
-                    //함께아는 친구 구하기 + 1명 이상이면 추천목록에 넣기
-                }else if(myFriendList.contains(dataSnapshot.getKey())){ //내 친구 목록에 있는 사람이라면
-                    //final ArrayList<String> yourFriendList = dataSnapshot.child("friendList").getValue(type);
+                String personKey = dataSnapshot.getKey();//personKey 하나 당 한 사람
+                if(!personKey.equals(myUid) && !myFriendList.contains(personKey)) {//나 자신과 이미 있는 친구 제외
+                    addCandidate(dataSnapshot);
+                //내 친구들의 친구목록으로 함께 아는 친구 해시맵 만들기
+                }else if(myFriendList.contains(personKey)){ //내 친구 목록에 있는 사람이라면
                     Log.e(TAG, "onChildAdded: 내 친구인 사람들의 모임입니다.");
                     UserDTO recomDTO = dataSnapshot.getValue(UserDTO.class);
-                    Log.e(TAG, recomDTO.name+"씨의 친구 목록을 꺼내볼까요" );
-
-                    for(String yourFriendUid : recomDTO.friendList){
-                        if(!yourFriendUid.equals(auth.getCurrentUser().getUid()) && !myFriendList.contains(yourFriendUid)){//그 친구가 나 이거나 이미 내 친구라면 제외함
-                            if (recommendMap.containsKey(yourFriendUid)) {//이미 있다면
-                                int count = recommendMap.get(yourFriendUid);
-                                count++; //출현 횟수를 한번 늘린다.
-                                Log.e(TAG, recomDTO.name+"씨 친구목록 안에 있습니다. "+ yourFriendUid+" "+count);
-                                recommendMap.put(yourFriendUid, count);
-                            } else {//아직 없다면
-                                recommendMap.put(yourFriendUid, 1);
-                                Log.e(TAG, recomDTO.name+"씨 친구목록 안에 있습니다. "+ yourFriendUid+" "+1);
-                                //최초로 등장했으므로 출현 횟수는 1
-                            }
-                            Log.e(TAG, "여기까지 완성된 해시맵: "+recommendMap);
-                            //이게 변경이 되는지는 모르겠다
-                            }
-                        }
-                    /*
-                    for (final String key : recommendMap.keySet()) {
-                        //Log.e(TAG, "onChildChanged: 추천 리스트 만드는 중...... " + key + " 키값 확인합니다. ");
-                        //Log.e(TAG, "userlist 사이즈가 설마..! " + userlist.size());
-                        //UserDTO userDTO = dataSnapshot.getValue(UserDTO.class);
-                        //Log.e(TAG, "onChildChanged: 추가될 userdto는?? " + userDTO.name);
-                        FriendItem item;
-                        for(UserDTO user : totalUserList){
-                            if(user.uid.equals(key)){
-                                item = new FriendItem(user.name, user.profileUrl, user.email, user.uid);
-                                if (!recommend_friends.contains(item)) {
-                                    recommend_friends.add(item);
-                                    recommend_adapter.notifyDataSetChanged();
-                                    Log.e(TAG, " 추천 친구 리스트를 만들고 notify했습니다" + recommend_friends);
-                                }
-                            }
-                        }
-                    }*/
+                    makeFriendMap(recomDTO, myUid);
                 }
             }
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                Log.e(TAG, "onChildChanged: is it called??!?!?!?!?!??!?!?");
-//                for (final String key : recommendMap.keySet()) {
-//                    //Log.e(TAG, "onChildChanged: 추천 리스트 만드는 중...... " + key + " 키값 확인합니다. ");
-//                    //Log.e(TAG, "userlist 사이즈가 설마..! " + userlist.size());
-//                    UserDTO userDTO = dataSnapshot.getValue(UserDTO.class);
-//                    Log.e(TAG, "onChildChanged: 추가될 userdto는?? " + userDTO.name);
-//                    FriendItem item = new FriendItem(userDTO.name, userDTO.profileUrl, userDTO.email, userDTO.uid);
-//                    if (!recommend_friends.contains(item)) {
-//                        recommend_friends.add(item);
-//                        recommend_adapter.notifyDataSetChanged();
-//                        Log.e(TAG, " 추천 친구 리스트를 만들고 notify했습니다" + recommend_friends);
-//                    }
-//                }
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
             }
 
             @Override
             public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
         });
 
-        database.getReference("userlist").addListenerForSingleValueEvent(new ValueEventListener() {
+        userListRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    Log.e(TAG, "onDataChange: 스냅샷 하나가 한 사람 아니에요?"+ snapshot);
-                    if(recommendMap.containsKey(snapshot.getKey())){
-                        Log.e(TAG, "onDataChange: 이사람 해시맵에 있었잖아요"+snapshot.getKey());
+                    String onePerson = snapshot.getKey();
+                    if(recommendMap.containsKey(onePerson)){//추천목록에 있는 사람이라면
                         UserDTO userDTO = snapshot.getValue(UserDTO.class);
-                        Log.e(TAG, "onDataChange: 이사람 정보 뽑아내고"+userDTO.uid);
                         FriendItem item = new FriendItem(userDTO.name,userDTO.profileUrl,userDTO.email,userDTO.uid);
-                        Log.e(TAG, "onDataChange: 아이템도 구성했어요 "+item.uid);
-                        if (!recommend_friends.contains(item)) {
-                            recommend_friends.add(item);
-                            //친구 순서로 정렬
-                            FriendItem temp ;
-                            for(int i = recommend_friends.size()-1; i>0; i--){
-                                for(int j = 0; j<i; j++){
-                                    if(recommendMap.get(recommend_friends.get(j).uid)<recommendMap.get(recommend_friends.get(j+1).uid)){
-                                        temp = recommend_friends.get(j);
-                                        recommend_friends.remove(j);
-                                        recommend_friends.add(j+1, temp);
-                                    }
-                                }
-                            }
+                        if (!recommendFriends.contains(item)) {
+                            recommendFriends.add(item);
+                            sortFriends();
                             recommend_adapter.notifyDataSetChanged();
-                            Log.e(TAG, " 추천 친구 리스트를 만들고 notify했습니다" + recommend_friends.size());
+                            Log.e(TAG, " 추천 친구 리스트를 만들고 notify했습니다" + recommendFriends.size());
                         }
                     }
                 }
@@ -411,17 +290,7 @@ public class ClubActivity extends MenuActivity {
             public boolean onQueryTextChange(String newText) {
                 if(newText != null && !newText.isEmpty()){
                     ArrayList<FriendItem> found = new ArrayList<>();
-                    for(FriendItem item: friendItems){
-                        if(newText.contains("@")) {
-                            String search_words[];
-                            search_words = newText.split("@");//검색어에 @ 가 포함되면 앞부분까지만 본다.
-                            newText = search_words[0];
-                        }
-                        String results[] = item.email.split("@");//각 사용자의 이메일도 앞부분까지만 본다
-                        if(item.name.contains(newText) || results[0].contains(newText)){//이메일에 검색어가 포함되거나 이름에 포함되면 추가
-                            found.add(item);
-                        }
-                    }
+                    putSearchedWord(found,newText);
                     FriendListAdapter adapter1 = new FriendListAdapter(ClubActivity.this, recommendMap, found);
                     Log.e(TAG, "onQueryTextChange: "+found.size());
                     friend_recycler.setAdapter(adapter1);
